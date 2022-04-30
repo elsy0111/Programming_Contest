4ffbfb5ecb11296bfda0d15f057ab14b4960dc1a
elsy0111 <kpp0193@gmail.com>
2022-04-30 22:16:07 +0900

    change_wav_analitics.py

--- Python_scripts/wav_analitics.py
+++ Python_scripts/wav_analitics.py
@@ -7,11 +7,15 @@ import matplotlib.pyplot as plt
 #音声ファイル読み込み
 wav_filename = "./audio/asano.wav"
 rate, data = scipy.io.wavfile.read(wav_filename)
+
+print("PCM sampling :",rate,data)
  
 ##### 音声データをそのまま表示する #####
-#縦軸（振幅）の配列を作成   #16bitの音声ファイルのデータを-1から1に正規化
-data = data / 32768
-#横軸（時間）の配列を作成　　#np.arange(初項, 等差数列の終点, 等差)
+#縦軸（振幅）の配列 = data   
+# #16bitの音声ファイルのデータを-32768 から 32767にプロット
+
+#横軸（時間）の配列 = time
+##np.arange(初項, 等差数列の終点, 等差)
 time = np.arange(0, data.shape[0]/rate, 1/rate)  
 #データプロット
 plt.plot(time, data)
