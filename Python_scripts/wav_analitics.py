285bc538fe39c1078a7e5d462d3c21339275bb13
elsy0111 <kpp0193@gmail.com>
2022-04-30 22:59:13 +0900

    modify code

--- Python_scripts/wav_analitics.py
+++ Python_scripts/wav_analitics.py
@@ -8,15 +8,23 @@ import matplotlib.pyplot as plt
 wav_filename = "./audio/asano.wav"
 rate, data = scipy.io.wavfile.read(wav_filename)
 
-print("PCM sampling :",rate,data)
- 
+PCM = rate
+T_Flame = data.shape[0]
+T_Time = T_Flame/rate
+
+print("PCM sampling :",PCM,"Hz")
+print("Total Flame :",T_Flame,"Flames")
+print("Total Time:Total Flame/Sampling")
+print(" = ",T_Time,"[s]")
+
 ##### 音声データをそのまま表示する #####
 #縦軸（振幅）の配列 = data   
 # #16bitの音声ファイルのデータを-32768 から 32767にプロット
 
 #横軸（時間）の配列 = time
-##np.arange(初項, 等差数列の終点, 等差)
+##np.arange(初項, 等差数列の終点, 等差(間隔))
 time = np.arange(0, data.shape[0]/rate, 1/rate)  
+
 #データプロット
 plt.plot(time, data)
 plt.show()
