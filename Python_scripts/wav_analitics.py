ac9ad1e10c1ed1d813c2231bb20d4b3a418ead12
elsy0111 <kpp0193@gmail.com>
2022-05-01 00:15:54 +0900

    modify 2d program

--- Python_scripts/wav_analitics.py
+++ Python_scripts/wav_analitics.py
@@ -15,15 +15,20 @@ T_Time = T_Flame/rate
 print("PCM sampling :",PCM,"Hz")
 print("Total Flame :",T_Flame,"Flames")
 print("Total Time:Total Flame/Sampling")
-print(" = ",T_Time,"[s]")
+print(" = ",T_Time,"sec")
 
 ##### 音声データをそのまま表示する #####
 #縦軸（振幅）の配列 = data   
 # #16bitの音声ファイルのデータを-32768 から 32767にプロット
 
 #横軸（時間）の配列 = time
-##np.arange(初項, 等差数列の終点, 等差(間隔))
-time = np.arange(0, data.shape[0]/rate, 1/rate)  
+##np.arange(初項 = 初めの時間[sec], 
+#           等差数列の終点　= 終わりの時間[sec], 
+#           等差[sec])
+time = np.arange(0, T_Time, 1/PCM)
+
+plt.xlabel("Time[s]")
+plt.ylabel("Loudness(bit)")
 
 #データプロット
 plt.plot(time, data)
