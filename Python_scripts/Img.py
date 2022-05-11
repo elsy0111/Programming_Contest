e08d86edda51a0395bfd675bad97aa95440d1251
sou31415 <tbc3141@icloud.com>
2022-05-11 15:43:05 +0900

    persentage

--- Python_scripts/Img.py
+++ Python_scripts/Img.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
 
-image1 = cv2.imread('xxx.png')  #画像ファイル名
-image2 = cv2.imread('yyy.png.png')  #比較する画像ファイル名
+image1 = cv2.imread('img1.png')  #画像ファイル名
+image2 = cv2.imread('img2.png')  #比較する画像ファイル名
 
 """
 比較する画像ファイルを複数用意する場合は
@@ -15,4 +15,4 @@ img_size = (200 , 200)  ##画像サイズの設定
 image1 = cv2.resize(image1 , img_size)  #画像ファイルのリサイズ
 image2 = cv2.resize(image2 , img_size)  #画像ファイルのリサイズ
 
-print(np.count_nonzero(image1 == image2) / image1.size) #画像の一致率を比較して出力
+print(np.count_nonzero(image1 == image2) * 100 / image1.size) #画像の一致率を比較して出力
