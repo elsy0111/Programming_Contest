e0b039415de0af6fdbb8bd6e31f785f629ae5a09
elsy0111 <kpp0193@gmail.com>
2022-07-02 11:01:35 +0900

    add offset

---
+++ Python_scripts/ffmpeg copy.py
@@ -0,0 +1,44 @@
+#-----IMPORT-----#
+import subprocess
+#-----IMPORT-----#
+
+
+list = [0,1,0,1,0,1]
+
+
+#--------------Make Script for Terminal--------------#
+n = 0
+str_list = ""
+
+for i,j in enumerate(list):
+	if j == 1:
+		if i%2 == 0: #日本語
+			i = int(i/2) + 1
+			if len(str(i)) == 1:
+				l = "J0" + str(i)
+			else:
+				l = "J" + str(i)
+		else:#英語
+			i = int(i/2) + 1
+			if len(str(i)) == 1:
+				l = "E0" + str(i)
+			else:
+				l = "E" + str(i)
+		str_list += "-i audio/Sample_Audio/"+l+".wav "
+		n += 1
+#--------------Make Script for Terminal--------------#
+
+
+#--------------Run on Terminal--------------#
+script = "ffmpeg "+str_list+"-filter_complex "\
+	'"[0]adelay=4800S|4800S[a];'\
+	'[1]adelay=9600S|9600S[b];'\
+	'[2]adelay=14400S|14400S[c];'\
+	'[a][b][c]amix=' +str(n)+'" -y audio/Conposition_Audio/out.wav'
+subprocess.run(script,shell = True)
+#--------------Run on Terminal--------------#
+
+
+print("--------SCRIPT--------")
+print(script)
+print("--------SCRIPT--------")
