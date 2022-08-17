f0423ea227b1e345eaddf275d7fe38a712d2cd13
elsy0111 <kpp0193@gmail.com>
2022-08-17 10:38:40 +0900

    cut_debugg

--- Python_scripts/test.py
+++ Python_scripts/test.py
@@ -1,5 +1,5 @@
 import subprocess
 
-#script = "ffmpeg -y -ss 50 -to 60 -i audio/debug.mp3 -c copy output.mp3"
+script = "ffmpeg -y -ss 50 -to 60 -i audio/debug.mp3 -c copy audio/output.mp3"
 
 subprocess.run(script,shell = True)
