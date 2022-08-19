9dd61f63d8330482e106f52bcca912ff39ca43fc
elsy0111 <kpp0193@gmail.com>
2022-08-19 09:35:30 +0900

    modify

--- Python_scripts/cut_audio.py
+++ Python_scripts/cut_audio.py
@@ -1,5 +1,12 @@
+from doctest import OutputChecker
 import subprocess
 
-script = "ffmpeg -y -i audio/debug.mp3 -af atrim=start_sample=44200:end_sample=120000 audio/output.mp3"
+# script = "ffmpeg -y -i audio/debug.mp3 -af atrim=start_sample=44200:end_sample=120000 audio/output.mp3"
+start_sample = 1
+end_sample = 96000
+out = 'audio/Conposition_Audio/split/out.wav'
+script = "ffmpeg -y -i audio/Conposition_Audio/out.wav -af atrim=start_sample=\
+" + str(start_sample) + ":end_sample=" + str(end_sample) + " " +  out
 
 subprocess.run(script,shell = True)
+print(script)
