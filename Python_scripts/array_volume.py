6d7886c9260976610558dd2900d62ed3a3dde060
elsy0111 <kpp0193@gmail.com>
2022-08-29 09:01:10 +0900

    array_volume

--- Python_scripts/array_volume.py
+++ Python_scripts/array_volume.py
@@ -1,20 +1,30 @@
 from scipy.io.wavfile import read , write
 import numpy as np
 
-writefile = "audio/test.wav"
-sample = read("audio/sample_Q_202205/sample_Q_202205/sample_Q_E01/sample_Q_E01/problem.wav")
+pcm,sample = read("audio/sample_Q_202205/sample_Q_202205/sample_Q_E01/sample_Q_E01/problem.wav")
+
 nspeech = ['E01' , 'E02' , 'E03'] #重なった読みデータ数
+
+#--------------MakeRandom------------------------
 element_list = []
 for i in nspeech:
     rater , datat = read("audio/Sample_Audio/"+i+".wav")
     element_list.append(len(datat))
-    
+#------------------------------------------------
+
+#--------------max_sample_length---------------- 
 new_list = sorted(element_list,reverse=True)
+
 print(new_list)
 print(element_list)
+
 max_element = new_list[0]
+
 print(max_element)
+#------------------------------------------------
+
 result = np.zeros(max_element)
+
 for i,name in enumerate(nspeech):
     rater , datat = read("audio/Sample_Audio/"+name+".wav")
     print("element:"+str(len(datat)))
@@ -24,12 +34,15 @@ for i,name in enumerate(nspeech):
     print(len(decoy))
     result += decoy
 
-#writefile = "audio/test.wav"
-
-#rate4, data4 = read("audio/sample_Q_202205/sample_Q_202205/sample_Q_E01/sample_Q_E01/problem.wav")
-
-data_a = np.array(result,dtype=np.float64)
-data_a /= 32768
-write(writefile,rate=rater,data=data_a)
+data_a = np.array(result,dtype=np.int16)
+sample = np.array(sample,dtype=np.int16)
 print(data_a)
-print(sample)
+print(len(sample))
+
+f = open('myfile.txt', 'w')
+f.write("data_a,sample\n")
+
+for i in range(225000,226816):
+    s = str(data_a[i])+" : "+str(sample[i]) 
+    f.write(s+'\n')
+f.close()
